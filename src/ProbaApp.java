e02e673476573c3c176928445fecc785c8ddaf00
Jovan P <janikfin@gmail.com>
2019-10-21 00:22:43 +0200

    za granu 2

--- src/ProbaApp.java
+++ src/ProbaApp.java
@@ -2,5 +2,6 @@ public class ProbaApp {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Stampa za drugi commit");
+        System.out.println("ovo je proba za stash");
     }
 }
